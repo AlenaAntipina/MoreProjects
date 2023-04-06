f1998b0a251f6d20416abbdab0fdb190eaa43adf
AlenaAntipina <38918308+AlenaAntipina@users.noreply.github.com>
2023-04-06 11:55:02 +0300

    Update test1.java

--- src/com/tinkoff/test/test1.java
+++ src/com/tinkoff/test/test1.java
@@ -4,6 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("test1");        
         System.out.println("test2");
-        System.out.println("3");
+        System.out.println("4");
     }
 }
