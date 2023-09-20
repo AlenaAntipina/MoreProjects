03533285cebef861bb19db2ed0371adb35f07fc1
AlenaAntipina <alena-antipina.antipina@yandex.ru>
2023-09-20 17:54:57 +0300

    add test2 and fix test1

--- src/com/tinkoff/test/test1.java
+++ src/com/tinkoff/test/test1.java
@@ -3,6 +3,5 @@ package com.tinkoff.test;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("test1");
-        System.out.println("test45");
     }
 }
---
+++ src/com/tinkoff/test/test2.java
@@ -0,0 +1,7 @@
+package com.tinkoff.test;
+
+public class test2 {
+    public void tryToDo() {
+
+    }
+}
