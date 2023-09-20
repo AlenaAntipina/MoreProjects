11bb58e3c668bb0732f2ed0d58a6c079dfd41e33
AlenaAntipina <alena-antipina.antipina@yandex.ru>
2023-09-20 17:38:36 +0300

    test commit final

--- src/com/tinkoff/test/test1.java
+++ src/com/tinkoff/test/test1.java
@@ -2,8 +2,7 @@ package com.tinkoff.test;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("test1");        
-        System.out.println("test2");
-        System.out.println("5");
+        System.out.println("test1");
+        System.out.println("test45");
     }
 }
